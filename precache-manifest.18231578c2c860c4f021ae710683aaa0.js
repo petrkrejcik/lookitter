0be5109bca2414bb0687f5dfca7e222134e4a776
Petr Krejčík <pkrejcik@soincon.es>
2020-10-17 00:54:55 +0200

    init

---
+++ precache-manifest.18231578c2c860c4f021ae710683aaa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014d5b4e24051f38175b2d2e8e93f5df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1941c9ce653716fb3e5b",
+    "url": "/static/css/main.0f951f6a.chunk.css"
+  },
+  {
+    "revision": "6479a33d8536028309e7",
+    "url": "/static/js/2.c7469b6c.chunk.js"
+  },
+  {
+    "revision": "e0447d07c581cfec01b69ae89d5eb5e8",
+    "url": "/static/js/2.c7469b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1941c9ce653716fb3e5b",
+    "url": "/static/js/main.8062883b.chunk.js"
+  },
+  {
+    "revision": "3111fa084ce0c4c504cf",
+    "url": "/static/js/runtime-main.a2a28aae.js"
+  }
+]);
